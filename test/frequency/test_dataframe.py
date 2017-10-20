1225c701f1b496485acf4340c1419748562d32e4
David Guzman <d.guzman@ucl.ac.uk>
2017-10-20 02:11:34 +0100

    Add container to be used as a dataframe in pandas

---
+++ test/frequency/test_dataframe.py
@@ -0,0 +1,10 @@
+from app.frequency import datalist
+import unittest
+
+
+class TestDataFrame(unittest.TestCase):
+
+    def test_date_and_stem_dataframe(self):
+        date_and_stem_df = datalist.DateAndStemDataList()
+        date_and_stem_df.append_to_datalist('Thu Oct 19 11:53:24 +0000 2017', ['test', 'tweet', '#alltestsmatter'])
+        self.assertListEqual(date_and_stem_df.datalist[2], ['Thu Oct 19 11:53:24 +0000 2017', '#alltestsmatter'])
